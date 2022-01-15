2753948e1228cf50630126f3e491c67271dd1062
Akif Islam <iamakifislam@gmail.com>
2022-01-15 20:15:22 +0600

    Learn Operator Overloading

---
+++ #12. January 2022/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+template<typename T> 
+// ostream& operator<<(ostream &os, const vector<T> &v) { 
+//     os << '{'; 
+//     for (const auto &x : v) os << gap << x;
+//         return os << '}'; 
+// }
